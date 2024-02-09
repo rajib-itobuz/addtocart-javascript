e30d7688793f5e10accf1012c37304ba624db86b
rajib-itobuz <rajib@itobuz.com>
2024-02-09 13:07:15 +0530

    wip added cart page

--- cartPage/cart.js
+++ cartPage/cart.js
@@ -1,56 +1,170 @@
 const cartData = localStorage.getItem("cart");
 const cart = cartData != "" && cartData != null ? JSON.parse(cartData) : [];
 const flexContainer = document.getElementById("flex-container");
+
 const cartCount = document.getElementById("cart-count");
-const invValue = document.createElement("h3")
-const discvalue = document.createElement("h3")
-const shipCharge = document.createElement("h3")
+const invValue = document.createElement("h3");
+const discvalue = document.createElement("h3");
+const shipCharge = document.createElement("h3");
+const totalAmt = document.createElement("h3");
+const checkOutBtn = document.createElement("button");
+
+cartCount.innerText = cart.reduce((acc, curr) => acc + curr.count, 0);
+
+const discount = 0.03;
+const shipping = 150;
+
+invValue.innerHTML = "<span>Cart Value :</span> <span>$0</span>";
+discvalue.innerHTML = "<span>Discount Applied :</span> <span>$0</span>";
+shipCharge.innerHTML = "<span>Shipping Charges :</span> <span>$0</span>";
+totalAmt.innerHTML = "<span>Total invoice Value :</span> <span>$0</span>";
+checkOutBtn.innerText = "Checkout";
+
+// functions
+const hideInvoiceSection = () => {
+  if (cart.length === 0) {
+    console.log(0);
+    totalPriceSection.innerHTML = "No Cart Items Added";
+    totalPriceSection.style.textAlign = "center";
+  }
+};
+const addtoCart = (item) => {
+  var index = cart.findIndex((cartItem) => cartItem.name === item.name);
+  if (index >= 0) {
+    cart[index].count += 1;
+  } else {
+    cart.push({
+      ...item,
+      count: 1,
+    });
+  }
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+  cartCount.innerText = cart.reduce((acc, curr) => acc + curr.count, 0);
+};
+
+const removefromCart = (item) => {
+  var index = cart.findIndex((cartItem) => cartItem.name === item.name);
+  if (index >= 0 && cart[index].count > 1) {
+    cart[index].count -= 1;
+  } else {
+    cart.splice(index, 1);
+  }
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+  cartCount.innerText = cart.reduce((acc, curr) => acc + curr.count, 0);
+};
 
 const totalPriceSection = document.getElementById("total-price");
 
 const updatePriceSection = (cart, discount, charges) => {
+  const cartAmountTotal = cart.reduce(
+    (a, curr) => a + curr.count * curr.price,
+    0
+  );
+  const discountValue = cartAmountTotal * discount;
+  invValue.innerHTML = `<span>Cart Value :</span> <span>${cartAmountTotal.toFixed(
+    2
+  )}</span>`;
+  discvalue.innerHTML = `<span>Discount Applied ${
+    discount * 100
+  }% :</span> <span>${discountValue.toFixed(2)}</span>`;
+  shipCharge.innerHTML = `<span>Shipping Charges :</span> <span>${charges}</span>`;
+  totalAmt.innerHTML = `<span>Total invoice Value :</span> <span>${(
+    cartAmountTotal -
+    discount +
+    charges
+  ).toFixed(2)}</span>`;
+};
 
-}
+totalPriceSection.append(
+  invValue,
+  discvalue,
+  shipCharge,
+  totalAmt,
+  checkOutBtn
+);
 
-// document.body.innerText = cart;
+cart.forEach((item) => {
+  const itemDiv = document.createElement("div");
+  itemDiv.classList.add("item");
 
-cart.forEach(item => {
-    const itemDiv = document.createElement("div");
-    itemDiv.classList.add("item")
+  const productImage = document.createElement("img");
+  productImage.src = item.imgUrl;
 
-    const productImage = document.createElement("img");
-    productImage.src = item.imgUrl;
+  const contentDiv = document.createElement("div");
+  contentDiv.classList.add("content");
 
+  const title = document.createElement("h5");
+  title.innerText =
+    item.name.length > 50 ? item.name.slice(0, 50) + "..." : item.name;
 
-    const contentDiv = document.createElement("div");
-    contentDiv.classList.add("content")
+  const price = document.createElement("h2");
+  price.innerText = item.price * item.count;
 
+  const cartCountDiv = document.createElement("div");
+  cartCountDiv.classList.add("cart-count");
 
-    const title = document.createElement("h5");
-    title.innerText =
-        item.name.length > 80 ? item.name.slice(0, 80) + "..." : item.name;
-    const price = document.createElement("h2");
-    price.innerText = item.price;
-    const quantity = document.createElement("h3");
-    quantity.innerText = item.count;
+  //+ - featrue
+  const btnAdd = document.createElement("div");
+  btnAdd.classList.add("btnAdd");
+  btnAdd.innerText = "+";
 
+  const count = document.createElement("div");
 
-    // add content to card
-    contentDiv.append(title, price, quantity)
-    itemDiv.append(productImage, contentDiv);
+  count.innerText = item.count > 0 ? item.count : 0;
+  count.style.fontSize = "25px";
 
-    //add to flex-container
-    flexContainer.appendChild(itemDiv);
+  const btnSub = document.createElement("div");
+  btnSub.innerText = "-";
+  btnSub.classList.add("btnSub");
 
+  //quantity
+  cartCountDiv.append(btnAdd, count, btnSub);
+  const quantity = document.createElement("h3");
+  quantity.append(cartCountDiv);
 
+  const crossButton = document.createElement("a");
+  crossButton.classList.add("cross-btn");
+  crossButton.innerText = "Remove";
 
+  // add content to card
+  contentDiv.append(title, price, quantity, crossButton);
+  itemDiv.append(productImage, contentDiv);
+
+  //add to flex-container
+  flexContainer.appendChild(itemDiv);
+
+  crossButton.addEventListener("click", () => {
+    cartItemIndex = cart.findIndex((cartItem) => cartItem === item);
+    cart.splice(cartItemIndex, 1);
+    flexContainer.removeChild(itemDiv);
+    price.innerText = item.price * item.count;
+    updatePriceSection(cart, discount, shipping);
+    hideInvoiceSection();
+  });
+
+  btnAdd.addEventListener("click", () => {
+    addtoCart(item);
+    price.innerText = item.price * item.count;
+    count.innerText = item.count;
+    updatePriceSection(cart, discount, shipping);
+  });
+
+  btnSub.addEventListener("click", () => {
+    cartItemIndex = cart.findIndex((cartItem) => cartItem === item);
+    const countItem = cart[cartItemIndex].count;
+    removefromCart(item);
+
+    price.innerText = item.price * item.count;
+
+    if (countItem == 1) {
+      flexContainer.removeChild(itemDiv);
+    } else count.innerText = item.count;
+    updatePriceSection(cart, discount, shipping);
+    hideInvoiceSection();
+  });
 });
 
-
-cartCount.innerText = cart.reduce((acc, curr) => acc + curr.count, 0);
-
-invValue.innerHTML = "Total invoice Value : <span>$0<span>"
-discvalue.innerHTML = "Discount Applied : <span>$0<span>"
-shipCharge.innerHTML = "Total invoice Value : <span>$0<span>"
-
-totalPriceSection.append(invValue, discvalue, shipCharge);
+updatePriceSection(cart, discount, shipping);
+hideInvoiceSection();
--- script.js
+++ script.js
@@ -1,147 +1,139 @@
 const flexContainer = document.getElementById("flex-container");
+const navbarCart = document.getElementById("navbar-cart");
+const cartCount = document.getElementById("cart-count");
 
 const dbData = localStorage.getItem("dbData");
 const cartData = localStorage.getItem("cart");
-const navbarCart = document.getElementById("navbar-cart");
-const cartCount = document.getElementById("cart-count");
+
 const data = dbData != "" && dbData != null ? JSON.parse(dbData) : [];
 const cart = cartData != "" && cartData != null ? JSON.parse(cartData) : [];
 
-
-navbarCart.addEventListener("click", () => {
-    window.location.href = "./cartPage/cart.html";
-});
-
+cartCount.innerText = cart.reduce((acc, curr) => acc + curr.count, 0);
 
 // localStorage.setItem("myCat", "Tom");
 const findinCart = (item) => {
-    return cart != []
-        ? cart.find((cartItem) => cartItem.name === item.name)
-        : null;
+  return cart != []
+    ? cart.find((cartItem) => cartItem.name === item.name)
+    : null;
+};
+
+const saveUpdate = () => {
+  localStorage.setItem("cart", JSON.stringify(cart));
+  cartCount.innerText = cart.reduce((acc, curr) => acc + curr.count, 0);
 };
 
 const addtoCart = (item) => {
-    var index = cart.findIndex((cartItem) => cartItem.name === item.name);
-    if (index >= 0) {
-        cart[index].count += 1;
-    } else {
-        cart.push({
-            ...item,
-            count: 1,
-        });
-    }
+  var index = cart.findIndex((cartItem) => cartItem.name === item.name);
+  if (index >= 0) {
+    cart[index].count += 1;
+  } else {
+    cart.push({
+      ...item,
+      count: 1,
+    });
+  }
 
-    localStorage.setItem("cart", JSON.stringify(cart));
-    cartCount.innerText = cart.reduce((acc, curr) => acc + curr.count, 0);
+  saveUpdate();
 };
 
 const removefromCart = (item) => {
-    var index = cart.findIndex((cartItem) => cartItem.name === item.name);
-    if (index >= 0 && cart[index].count > 1) {
-        cart[index].count -= 1;
-    } else {
-        cart.splice(index, 1);
-    }
+  var index = cart.findIndex((cartItem) => cartItem.name === item.name);
+  if (index >= 0 && cart[index].count > 1) {
+    cart[index].count -= 1;
+  } else {
+    cart.splice(index, 1);
+  }
 
-    localStorage.setItem("cart", JSON.stringify(cart));
-    cartCount.innerText = cart.reduce((acc, curr) => acc + curr.count, 0);
+  saveUpdate();
 };
 
 data.forEach((item, ind) => {
-    // create item
-    const itemDiv = document.createElement("div");
-    itemDiv.classList.add("item");
+  // create item
+  const itemDiv = document.createElement("div");
+  itemDiv.classList.add("item");
 
-    // // add item fields
-    const productImage = document.createElement("img");
-    productImage.src = item.imgUrl;
-    const title = document.createElement("h5");
-    title.innerText =
-        item.name.length > 50 ? item.name.slice(0, 50) + "..." : item.name;
-    const price = document.createElement("h2");
-    price.innerText = item.price;
+  // // add item fields
+  const productImage = document.createElement("img");
+  productImage.src = item.imgUrl;
 
-    // button
-    const button = document.createElement("button");
-    button.innerText = "Add to Cart";
-    button.classList.add("btn-cart");
+  const title = document.createElement("h5");
+  title.innerText =
+    item.name.length > 50 ? item.name.slice(0, 50) + "..." : item.name;
 
-    // // card count div
-    const cartCountDiv = document.createElement("div");
-    cartCountDiv.classList.add("cart-count");
+  const price = document.createElement("h2");
+  price.innerText = item.price;
 
-    const btnAdd = document.createElement("div");
-    btnAdd.classList.add("btnAdd");
-    btnAdd.innerText = "+";
+  // button
+  const button = document.createElement("button");
+  button.innerText = "Add to Cart";
+  button.classList.add("btn-cart");
 
-    const count = document.createElement("div");
-    let cartItemIndex = cart.findIndex(
-        (cartItem) => cartItem.name === item.name
-    );
-    count.innerText = cartItemIndex >= 0 ? cart[cartItemIndex].count : 0;
-    count.style.fontSize = "25px";
+  // // card count div
+  const cartCountDiv = document.createElement("div");
+  cartCountDiv.classList.add("cart-count");
 
-    const btnSub = document.createElement("div");
-    btnSub.innerText = "-";
-    btnSub.classList.add("btnSub");
+  const btnAdd = document.createElement("div");
+  btnAdd.classList.add("btnAdd");
+  btnAdd.innerText = "+";
 
+  const count = document.createElement("div");
+  let cartItemIndex = cart.findIndex((cartItem) => cartItem.name === item.name);
+  count.innerText = cartItemIndex >= 0 ? cart[cartItemIndex].count : 0;
+  count.style.fontSize = "25px";
 
-    //on normal rendering
-    if (findinCart(item) == null) {
-        cartCountDiv.style.display = "none";
-        button.style.display = "block";
-    } else {
-        cartCountDiv.style.display = "flex";
-        button.style.display = "none";
-    }
+  const btnSub = document.createElement("div");
+  btnSub.innerText = "-";
+  btnSub.classList.add("btnSub");
 
+  //on normal rendering
+  if (findinCart(item) == null) {
+    cartCountDiv.style.display = "none";
+    button.style.display = "block";
+  } else {
+    cartCountDiv.style.display = "flex";
+    button.style.display = "none";
+  }
 
+  //on event
+  button.addEventListener("click", () => {
+    // button.innerText="Go to Cart"
+    addtoCart(item);
+    cartCountDiv.style.display = "flex";
+    button.style.display = "none";
 
-    //on event
-    button.addEventListener("click", () => {
-        // button.innerText="Go to Cart"
-        addtoCart(item);
-        cartCountDiv.style.display = "flex";
-        button.style.display = "none";
-        cartItemIndex = cart.findIndex(
-            (cartItem) => cartItem.name === item.name
-        );
-        count.innerText = cartItemIndex >= 0 ? cart.find((cartItem) => cartItem.name === item.name).count : 0;
-    });
+    count.innerText = item.index;
+  });
 
-    btnAdd.addEventListener("click", () => {
-        addtoCart(item);
-        cartItemIndex = cart.findIndex(
-            (cartItem) => cartItem.name === item.name
-        );
-        count.innerText = cartItemIndex >= 0 ? cart.find((cartItem) => cartItem.name === item.name).count : 0;
+  btnAdd.addEventListener("click", () => {
+    addtoCart(item);
 
+    count.innerText = item.index;
+  });
 
-    });
+  btnSub.addEventListener("click", () => {
+    // console.log(cart.find((cartItem) => cartItem.name === item.name).count);
+    cartItemIndex = cart.findIndex((cartItem) => cartItem.name === item.name);
+    const countItem = cart[cartItemIndex].count;
+    removefromCart(item);
+    if (countItem == 1) {
+      cartCountDiv.style.display = "none";
+      button.style.display = "block";
+    } else
+      count.innerText =
+        cartItemIndex >= 0
+          ? cart.find((cartItem) => cartItem.name === item.name).count
+          : 0;
+    // count.innerText = cartItemIndex >= 0 ? cart.find((cartItem) => cartItem.name === item.name).count : 0;
+  });
 
-    btnSub.addEventListener("click", () => {
-        // console.log(cart.find((cartItem) => cartItem.name === item.name).count);
-        cartItemIndex = cart.findIndex(
-            (cartItem) => cartItem.name === item.name
-        );
-        const countItem = cart[cartItemIndex].count
-        removefromCart(item);
-        if (countItem == 1) {
-            cartCountDiv.style.display = "none";
-            button.style.display = "block";
-        }
-        else
-            count.innerText = cartItemIndex >= 0 ? cart.find((cartItem) => cartItem.name === item.name).count : 0;
-        // count.innerText = cartItemIndex >= 0 ? cart.find((cartItem) => cartItem.name === item.name).count : 0;
+  // // add content to item
+  cartCountDiv.append(btnAdd, count, btnSub);
+  itemDiv.append(productImage, title, price, button, cartCountDiv);
 
-    })
-
-    // // add content to item
-    cartCountDiv.append(btnAdd, count, btnSub);
-    itemDiv.append(productImage, title, price, button, cartCountDiv);
-
-    // // add to flex-container
-    flexContainer.appendChild(itemDiv);
+  // // add to flex-container
+  flexContainer.appendChild(itemDiv);
 });
 
-cartCount.innerText = cart.reduce((acc, curr) => acc + curr.count, 0);
+navbarCart.addEventListener("click", () => {
+  window.location.href = "./cartPage/cart.html";
+});
