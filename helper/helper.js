8b6ffb1588aa192b9aa9f893baeab00c738d7aca
Rajib <rajib@itobuz.com>
2024-02-12 06:50:22 +0530

    reusability complete with addeventlistener

--- helper/helper.js
+++ helper/helper.js
@@ -1,18 +1,69 @@
 const dbData = localStorage.getItem("dbData");
 const cartData = localStorage.getItem("cartData");
+
 export const data = dbData != "" && dbData != null ? JSON.parse(dbData) : [];
 export const cart =
   cartData != "" && cartData != null ? JSON.parse(cartData) : [];
-export const cartCount = document.getElementById("cart-count");
+const cartCount = document.getElementById("cart-count");
 
 cartCount.innerText = cart.reduce((acc, curr) => acc + curr.count, 0);
 
-export const saveUpdate = () => {
+///cart section
+const totalPriceSection = document.getElementById("total-price");
+const discount = 0.03;
+const shipping = 150;
+const invValue = document.createElement("h3");
+const discvalue = document.createElement("h3");
+const shipCharge = document.createElement("h3");
+const totalAmt = document.createElement("h3");
+const checkOutBtn = document.createElement("button");
+export const hideInvoiceSection = () => {
+  if (cart.length === 0) {
+    totalPriceSection.innerHTML = "No Cart Items Added";
+    totalPriceSection.style.textAlign = "center";
+  }
+};
+export const updatePriceSection = (cart, discount, charges) => {
+  const cartAmount = cart.reduce((a, curr) => a + curr.count * curr.price, 0);
+  const discountValue = cartAmount * discount;
+  invValue.innerHTML = `<span>Cart Value :</span> <span>${cartAmount.toFixed(
+    2
+  )}</span>`;
+  discvalue.innerHTML = `<span>Discount Applied ${
+    discount * 100
+  }% :</span> <span>${discountValue.toFixed(2)}</span>`;
+  shipCharge.innerHTML = `<span>Shipping Charges :</span> <span>${charges}</span>`;
+  totalAmt.innerHTML = `<span>Total invoice Value :</span> <span>${(
+    cartAmount -
+    discountValue +
+    charges
+  ).toFixed(2)}</span>`;
+};
+if (totalPriceSection) {
+  invValue.innerHTML = "<span>Cart Value :</span> <span>0</span>";
+  discvalue.innerHTML = "<span>Discount Applied :</span> <span>0</span>";
+  shipCharge.innerHTML = "<span>Shipping Charges :</span> <span>0</span>";
+  totalAmt.innerHTML = "<span>Total invoice Value :</span> <span>0</span>";
+  checkOutBtn.innerText = "Checkout";
+
+  totalPriceSection.append(
+    invValue,
+    discvalue,
+    shipCharge,
+    totalAmt,
+    checkOutBtn
+  );
+
+  hideInvoiceSection();
+  updatePriceSection(cart, discount, shipping);
+}
+
+const saveUpdate = () => {
   localStorage.setItem("cartData", JSON.stringify(cart));
   cartCount.innerText = cart.reduce((acc, curr) => acc + curr.count, 0);
 };
 
-export const addtoCart = (item) => {
+const addtoCart = (item, cartCountDiv, button) => {
   var index = cart.findIndex((cartItem) => cartItem.name === item.name);
   if (index >= 0) {
     cart[index].count += 1;
@@ -21,33 +72,145 @@ export const addtoCart = (item) => {
       ...item,
       count: 1,
     });
-  }
 
-  saveUpdate();
+    cartCountDiv.style.display = "flex";
+    button.style.display = "none";
+  }
 };
 
-export const removefromCart = (item) => {
+const removefromCart = (item, cartCountDiv, button) => {
   var index = cart.findIndex((cartItem) => cartItem.name === item.name);
   if (index >= 0 && cart[index].count > 1) {
     cart[index].count -= 1;
   } else {
     cart.splice(index, 1);
+    cartCountDiv.style.display = "none";
+    button.style.display = "block";
   }
-
-  saveUpdate();
 };
 
-export const createItemDiv = (item) => {
-  // product image edit
+const deletefromCart=(item)=>{
+  cart.splice(cart.findIndex((cartItem) => cartItem === item), 1);
+}
+
+const findinCart = (item) =>
+  cart != [] ? cart.find((cartItem) => cartItem.name === item.name) : null;
+
+export const createItem = (item, flexContainer) => {
+  const itemDiv = document.createElement("div");
+  itemDiv.classList.add("item");
+
+  // // add item fields
   const productImage = document.createElement("img");
   productImage.src = item.imgUrl;
 
-  // title image edit
+  const contentDiv = document.createElement("div");
+  contentDiv.classList.add("content");
+
   const title = document.createElement("h5");
   title.innerText =
     item.name.length > 50 ? item.name.slice(0, 50) + "..." : item.name;
 
   const price = document.createElement("h2");
-  price.innerText = item.price;
-  return {};
+  price.innerText = item.count ? item.count * item.price : item.price;
+
+  // button
+  const button = document.createElement("button");
+  button.innerText = "Add to Cart";
+  button.classList.add("btn-cart");
+
+  // // card count div
+  const cartCountDiv = document.createElement("div");
+  cartCountDiv.classList.add("cart-count");
+
+  const btnAdd = document.createElement("div");
+  btnAdd.classList.add("btnAdd");
+  btnAdd.innerText = "+";
+
+  const count = document.createElement("div");
+  const cartItemIndex = cart.findIndex(
+    (cartItem) => cartItem.name === item.name
+  );
+  count.innerText = cartItemIndex >= 0 ? cart[cartItemIndex].count : 1;
+  count.style.fontSize = "25px";
+
+  const btnSub = document.createElement("div");
+  btnSub.innerText = "-";
+  btnSub.classList.add("btnSub");
+
+  const crossButton = document.createElement("a");
+  crossButton.classList.add("cross-btn");
+  crossButton.innerText = "Remove";
+
+  cartCountDiv.append(btnSub, count, btnAdd, crossButton);
+  const quantity = document.createElement("h3");
+  quantity.append(cartCountDiv);
+
+  // add content to card
+  contentDiv.append(title, price, button, quantity);
+
+  itemDiv.append(productImage, contentDiv);
+
+  if (findinCart(item) == null) {
+    cartCountDiv.style.display = "none";
+    button.style.display = "block";
+  } else {
+    cartCountDiv.style.display = "flex";
+    button.style.display = "none";
+  }
+
+  flexContainer.appendChild(itemDiv);
+
+  onEventCallback(button, "add", item, button, cartCountDiv, count);
+  onEventCallback(
+    btnSub,
+    "remove",
+    item,
+    button,
+    cartCountDiv,
+    count,
+    price,
+    flexContainer,
+    itemDiv
+  );
+  onEventCallback(
+    crossButton,
+    "delete",
+    item,
+    button,
+    cartCountDiv,
+    count,
+    price,
+    flexContainer,
+    itemDiv
+  );
+  onEventCallback(btnAdd, "add", item, button, cartCountDiv, count, price);
+};
+
+const mapmyFunction = {
+  add: addtoCart,
+  remove: removefromCart,
+  delete:deletefromCart,
+};
+
+const onEventCallback = (button, callbackName, item, ...argv) => {
+  button.addEventListener("click", () => {
+    mapmyFunction[callbackName](item, argv[1], argv[0]);
+    const cartItemIndex = cart.findIndex(
+      (cartItem) => cartItem.name === item.name
+    );
+    const countItem = cartItemIndex >= 0 ? cart[cartItemIndex].count : 0;
+    argv[2].innerText = countItem;
+
+
+    if (item.count) {
+      argv[3].innerText = item.price * item.count;
+      updatePriceSection(cart, discount, shipping);
+      
+      if (cartItemIndex == -1) argv[4].removeChild(argv[5]);
+      hideInvoiceSection();
+    }
+
+    saveUpdate();
+  });
 };
--- script.js
+++ script.js
@@ -1,94 +1,12 @@
-import { cart, data, addtoCart, removefromCart } from "./helper/helper.js";
+import {
+  data,createItem,
+} from "./helper/helper.js";
 
 const flexContainer = document.getElementById("flex-container");
 const navbarCart = document.getElementById("navbar-cart");
 
 // localStorage.setItem("myCat", "Tom");
-const findinCart = (item) =>
-  cart != [] ? cart.find((cartItem) => cartItem.name === item.name) : null;
-
-data.forEach((item, ind) => {
-  // create item
-  const itemDiv = document.createElement("div");
-  itemDiv.classList.add("item");
-
-  // // add item fields
-  const productImage = document.createElement("img");
-  productImage.src = item.imgUrl;
-
-  const title = document.createElement("h5");
-  title.innerText =
-    item.name.length > 50 ? item.name.slice(0, 50) + "..." : item.name;
-
-  const price = document.createElement("h2");
-  price.innerText = item.price;
-
-  // button
-  const button = document.createElement("button");
-  button.innerText = "Add to Cart";
-  button.classList.add("btn-cart");
-
-  // // card count div
-  const cartCountDiv = document.createElement("div");
-  cartCountDiv.classList.add("cart-count");
-
-  const btnAdd = document.createElement("div");
-  btnAdd.classList.add("btnAdd");
-  btnAdd.innerText = "+";
-
-  const count = document.createElement("div");
-  let cartItemIndex = cart.findIndex((cartItem) => cartItem.name === item.name);
-  count.innerText = cartItemIndex >= 0 ? cart[cartItemIndex].count : 0;
-  count.style.fontSize = "25px";
-
-  const btnSub = document.createElement("div");
-  btnSub.innerText = "-";
-  btnSub.classList.add("btnSub");
-
-  //on normal rendering
-  if (findinCart(item) == null) {
-    cartCountDiv.style.display = "none";
-    button.style.display = "block";
-  } else {
-    cartCountDiv.style.display = "flex";
-    button.style.display = "none";
-  }
-
-  // // add content to item
-  cartCountDiv.append(btnSub, count, btnAdd);
-  itemDiv.append(productImage, title, price, button, cartCountDiv);
-
-  // // add to flex-container
-  flexContainer.appendChild(itemDiv);
-
-  //on event
-  button.addEventListener("click", () => {
-    addtoCart(item);
-    cartCountDiv.style.display = "flex";
-    button.style.display = "none";
-    cartItemIndex = cart.findIndex((cartItem) => cartItem.name === item.name);
-    count.innerText =
-      cartItemIndex >= 0
-        ? cart.find((cartItem) => cartItem.name === item.name).count
-        : 0;
-  });
-
-  btnAdd.addEventListener("click", () => {
-    addtoCart(item);
-    cartItemIndex = cart.findIndex((cartItem) => cartItem.name === item.name);
-    count.innerText = cartItemIndex >= 0 ? cart[cartItemIndex].count : 0;
-  });
-
-  btnSub.addEventListener("click", () => {
-    cartItemIndex = cart.findIndex((cartItem) => cartItem.name === item.name);
-    const countItem = cart[cartItemIndex].count;
-    removefromCart(item);
-    if (countItem == 1) {
-      cartCountDiv.style.display = "none";
-      button.style.display = "block";
-    } else count.innerText = cartItemIndex >= 0 ? cart[cartItemIndex].count : 0;
-  });
-});
+data.forEach((item) =>createItem(item,flexContainer));
 
 navbarCart.addEventListener("click", () => {
   window.location.href = "./cartPage/cart.html";
